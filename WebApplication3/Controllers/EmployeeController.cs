42bbbd43683e90cf2b6b51cb100125f8b37cd3bd
205-ChalimamidiHemanthKumar <imhemanth5@gmail.com>
2021-12-30 20:35:19 -0800

    webapplications3

---
+++ WebApplication3/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public string Index()
+        {
+            return "Employee Name";
+        }
+        public string FirstName()
+        {
+            return "Employee FirstName: Hemanth";
+        }
+        public string LastName()
+        {
+            return "Employee LastName: Kumar";
+        }
+    }
+}
---
+++ WebApplication3/Controllers/HelloworldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        public string Index()
+        {
+            return "This is the default Action";
+        }
+        public string welcome()
+        {
+            return "This is a welcome action";
+        }
+
+    }
+}
